119bcbb80996ecc60f775920d947dd097b4e6174
Mohamed Mansour <moyamed2mansour@gmail.com>
2023-08-15 23:18:51 +0300

    done

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,2 @@
+-- SQL script that lists all rows of the table first_table
+select * FROM first_table;
---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,2 @@
+-- SQL script that inserts a new row in the table first_table
+INSERT INTO first_table ('id', 'name') values ('89', 'Best School')
