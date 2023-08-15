9646c3cec75f4d7c7224124a23d5d3e595a897be
Mohamed Mansour <moyamed2mansour@gmail.com>
2023-08-15 22:34:43 +0300

    doo

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,2 @@
-script that creates a table called first_table in the current database in your MySQL server.
-CREATE TABLES IF NOT Exist first_table (
-    id INT
-    name VARCHAR(256)
-);
+--script that creates a table called first_table in the current database in your MySQL server.
+CREATE TABLES IF NOT Exist first_table (id INT , name VARCHAR(256));
