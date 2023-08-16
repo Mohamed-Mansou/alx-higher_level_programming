f643097c146b85f390edc2503da26a2d7c76996d
Mohamed Mansour <moyamed2mansour@gmail.com>
2023-08-16 21:31:47 +0300

    done

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,5 @@
+-- SQL script that creates the database hbtn_0d_usa and the table states
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa ;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
+    id INT UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL);
