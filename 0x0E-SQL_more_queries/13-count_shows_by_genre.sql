293fc7eb25071f3048b4ee4494a4fa73ff0e0831
Mohamed Mansour <moyamed2mansour@gmail.com>
2023-08-16 23:11:03 +0300

    ADD

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,9 +1,4 @@
--- lists all genres contained in hbtn_0d_ and the number of shows linked to each
-   -- Each record should display:
-      -- <TV Show genre> - <Number of shows linked to this genre>
-   -- Results must be sorted in descending order by the number of shows linkeda
-   -- The database name will be passed as an argument of the mysql command
-
+-- SQL lists all genres contained in hbtn_0d_ and the number of shows linked to each
 SELECT name AS genre, COUNT(*) AS number_of_shows FROM tv_genres
 JOIN tv_show_genres ON id=tv_show_genres.genre_id
 GROUP BY tv_show_genres.genre_id
--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,10 +1,4 @@
--- lists all Comedy shows in the database hbtn_0d_tvshows
--- displays the number of shows linked to each
-   -- Each record should display:
-      -- tv_shows.title
-   -- Results must be sorted in ascending order by the show title
-   -- The database name will be passed as an argument of the mysql command
-
+-- SQL lists all Comedy shows in the database hbtn_0d_tvshows
 SELECT title FROM tv_shows
 JOIN tv_show_genres ON id=tv_show_genres.show_id
 JOIN tv_genres ON tv_genres.id=tv_show_genres.genre_id
